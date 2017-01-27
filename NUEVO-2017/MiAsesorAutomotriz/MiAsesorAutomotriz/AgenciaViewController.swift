e8680cae35d04514ab3bef39c990f2b080a94487
mvasco <modestovasco@gmail.com>
2017-01-26 18:24:40 -0700

    BOILERPLATE PASO A MACINCLOUD

--- NUEVO-2017/MiAsesorAutomotriz/MiAsesorAutomotriz/AgenciaViewController.swift
+++ NUEVO-2017/MiAsesorAutomotriz/MiAsesorAutomotriz/AgenciaViewController.swift
@@ -1,25 +1,48 @@
 //
-//  ViewController.swift
+//  AgenciaViewController.swift
 //  MiAsesorAutomotriz
 //
-//  Created by MODESTO VASCO FORNAS on 1/23/17.
+//  Created by MODESTO VASCO FORNAS on 1/25/17.
 //  Copyright © 2017 Modesto Vasco. All rights reserved.
 //
 
 import UIKit
 
-class AgenciaViewController: UIViewController {
+class AgenciaViewController: UIViewController, UITextFieldDelegate  {
 
+    @IBOutlet weak var codigoTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.codigoTextField.delegate = self
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
 
+    @IBAction func buscarButtonAction(_ sender: Any) {
+        
+        
+        let codigo = codigoTextField.text
+        
+        if (codigo?.isEmpty)!{
+            //display alert
+            
+            let myAlert = UIAlertController(title:"Aviso", message:"Debes escribir un código de Agencia",preferredStyle:UIAlertControllerStyle.alert);
+            let okAction = UIAlertAction(title:"Cerrar", style:UIAlertActionStyle.default, handler:nil)
+            myAlert.addAction(okAction);
+            self.present(myAlert, animated:true,completion:nil)
+            
+            return
+        }
+    }
 }
-
--- NUEVO-2017/MiAsesorAutomotriz/MiAsesorAutomotriz/ViewController.swift
+++ NUEVO-2017/MiAsesorAutomotriz/MiAsesorAutomotriz/ViewController.swift
@@ -10,14 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var agenciaLabel: UILabel!
+    
+    @IBOutlet weak var asesorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+          print ("estoy en viewDidLoad")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+       
+        if let x = UserDefaults.standard.object(forKey:"miAgencia") as? String
+        {
+            agenciaLabel.text = x
+        }
+        else
+        {
+            agenciaLabel.text = "No has seleccionado ninguna Agencia"
+        }
+        if let y = UserDefaults.standard.object(forKey:"miAsesor") as? String
+        {
+            asesorLabel.text = y
+        }
+        else
+        {
+            asesorLabel.text = "No has seleccionado ningún Asesor"
+        }
     }
 
 
