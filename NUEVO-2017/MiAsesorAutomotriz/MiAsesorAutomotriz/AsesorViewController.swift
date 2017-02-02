b1eb53861591383109131a538b7bb6ef0f20d6d1
Unknown <desarrollo@webjuarez.com>
2017-02-02 16:39:03 -0700

    LAYOUT DE ASESORVIEWCONTROLLER

---
+++ NUEVO-2017/MiAsesorAutomotriz/MiAsesorAutomotriz/AsesorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AsesorViewController.swift
+//  MiAsesorAutomotriz
+//
+//  Created by MODESTO VASCO FORNAS on 2/2/17.
+//  Copyright © 2017 Modesto Vasco. All rights reserved.
+//
+
+import UIKit
+
+class AsesorViewController: UIViewController {
+
+    @IBOutlet weak var asesoresPickerView: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
