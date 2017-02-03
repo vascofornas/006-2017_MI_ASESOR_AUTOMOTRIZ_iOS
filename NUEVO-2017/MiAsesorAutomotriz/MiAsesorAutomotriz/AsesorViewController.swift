7146fae2c9728efcb5dfe16195a746d315b027c7
Unknown <desarrollo@webjuarez.com>
2017-02-02 19:06:14 -0700

    ASESORVC

--- NUEVO-2017/MiAsesorAutomotriz/MiAsesorAutomotriz/AsesorViewController.swift
+++ NUEVO-2017/MiAsesorAutomotriz/MiAsesorAutomotriz/AsesorViewController.swift
@@ -8,21 +8,50 @@
 
 import UIKit
 
-class AsesorViewController: UIViewController {
+class AsesorViewController: UIViewController,UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
-    @IBOutlet weak var asesoresPickerView: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
+       
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+ 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     
+    {
+        return 1
+    }
+   
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
 
+    {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell",for: indexPath)
+        myCell.textLabel?.text = "probando"
+        myCell.textLabel?.textColor = UIColor.gray
+      
+        myCell.textLabel?.font = UIFont(name:"Avenir", size:22)
+        return myCell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        print ("He pulsado la celda \(indexPath.row)")
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+    {
+        print ("USre typed \(searchBar.text)")
+    }
 }
