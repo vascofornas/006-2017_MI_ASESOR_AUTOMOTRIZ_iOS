f2fa86ca6a37ed8cdb07e2ae0621e1c1c71248c6
vascofornas <desarrollo@webjuarez.com>
2017-08-16 22:58:08 -0600

    IOS - COMPROBAR ENVIO CORREOS

--- NUEVO-2017/php files actuales/enviarCita.php
+++ NUEVO-2017/php files actuales/enviarCita.php
@@ -50,11 +50,10 @@ $from = 'Mi Asesor Automotriz';
 // To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- 
-// Create email headers
-$headers .= 'From: '.$from."\r\n".
-    'Reply-To: '.$from."\r\n" .
+$headers .= 'From: soporte@miasesorautomotriz.com' . "\r\n" .
+    'Reply-To: soporte@miasesorautomotriz.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
+// Compose a simple HTML email message
  
 // Compose a simple HTML email message
 $message = '<html><body>';
@@ -90,11 +89,10 @@ $from = 'Mi Asesor Automotriz';
 // To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- 
-// Create email headers
-$headers .= 'From: '.$from."\r\n".
-    'Reply-To: '.$from."\r\n" .
+$headers .= 'From: soporte@miasesorautomotriz.com' . "\r\n" .
+    'Reply-To: soporte@miasesorautomotriz.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
+// Compose a simple HTML email message
  
 // Compose a simple HTML email message
 $message = '<html><body>';
@@ -130,11 +128,10 @@ $from = 'Mi Asesor Automotriz';
 // To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- 
-// Create email headers
-$headers .= 'From: '.$from."\r\n".
-    'Reply-To: '.$from."\r\n" .
+$headers .= 'From: soporte@miasesorautomotriz.com' . "\r\n" .
+    'Reply-To: soporte@miasesorautomotriz.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
+// Compose a simple HTML email message
  
 // Compose a simple HTML email message
 $message = '<html><body>';
